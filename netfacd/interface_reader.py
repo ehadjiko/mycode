4e26430d74ca75309e0801b5eac31f63c28fb50b
Steve <steve@example.com>
2022-03-09 19:44:56 +0000

    Python and physical interfaces

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -2,20 +2,34 @@
 
 import netifaces
 
-print(netifaces.interfaces())
+def macgetter(interface_name):
+    """pass an interface name (string) will return the MAC"""
+    mac = netiface.ifaddresses(interface_name)[netifeces.AF_LINK][0]["adde"]
+    return mac #string
 
-for i in netifaces.interfaces():
-    print(netifaces.ifaddresses(i))
+def ipgetter(interface_name):
+    """pass an interface name (string) will return IP"""
+    ip = netifaces.ifaddresses(interface_name)[netifaces.AF_INET][0]['addr']
+    return ip # string
+
+def main():
+    print(netifaces.interfaces())
+
+    for i in netifaces.interfaces():
+
     print('\n**************Details of Interface - ' + i + ' *********************')
     try:
-        print('MAC: ', end="")  # This print statement will always print MAC without an end of line
-        print((netifaces, ifaddresses(i) [netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
+        print('MAC: ', end='')  # This print statement will always print MAC without an end of line
+        print(macgetter(i) ) # Prints the MAC address
         print('IP: ', end='')  # This print statement will always print IP without an end of line
-        print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
+        print(ipgetter(i) )  # This print statement will always print IP without an end of line
+        
     except:          # This is a new line
         print('Could not collect adapter information') # Print an error message 
 
-
+if __name__ "__main__:
+    main()
+    
 
 
 
